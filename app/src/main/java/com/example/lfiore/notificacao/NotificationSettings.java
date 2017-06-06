ec219d361095ae616f7cd3fa4903fdde142ba81b
Leonardo Ribeiro Fiore <lfiore@micromidia-1.local>
2017-06-06 15:33:37 -0300

    Up

---
+++ app/src/main/java/com/example/lfiore/notificacao/NotificationSettings.java
@@ -0,0 +1,12 @@
+package com.example.lfiore.notificacao;
+
+/**
+ * Created by lfiore on 23/05/2017.
+ */
+
+public class NotificationSettings {
+
+    public static String SenderId = "796407961201";
+    public static String HubName = "Notificacao";
+    public static String HubListenConnectionString = "Endpoint=sb://notificacaonamespace.servicebus.windows.net/;SharedAccessKeyName=DefaultListenSharedAccessSignature;SharedAccessKey=+IxW7k8y7PJg8qgDSiE0UZiUy7k410cnOeJjQQ0w5qY=";
+}
